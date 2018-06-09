ce4f2eafac148932dfe1e17610aacd6f4b5915fb
paramagicdev <konnor5456@gmail.com>
2018-06-09 18:56:15 -0400

    Added .rspec file

--- spec/paramagic_chess_spec.rb
+++ spec/paramagic_chess_spec.rb
@@ -1,9 +1,12 @@
-RSpec.describe ParamagicChess do
+module ParamagicChess
+  RSpec.describe do
+
   it "has a version number" do
-    expect(ParamagicChess::VERSION).not_to be nil
+    expect(VERSION).not_to be nil
   end
 
   it "does something useful" do
     expect(true).to eq(true)
   end
+end
 end
