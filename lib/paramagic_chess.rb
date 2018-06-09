64a6b5dc4cd71bcaf2af313109908bafbd058ad9
paramagicdev <konnor5456@gmail.com>
2018-06-09 03:06:27 -0400

    added require statements for paramagic_chess.rb

--- lib/paramagic_chess.rb
+++ lib/paramagic_chess.rb
@@ -1,4 +1,7 @@
 require "paramagic_chess/version"
+require 'paramagic_chess/board'
+require 'paramagic_chess/piece'
+require 'paramagic_chess/tile'
 
 module ParamagicChess
   # Your code goes here...
