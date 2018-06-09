51d488e68f35ee94a515d505b844fa19dee3dbbf
paramagicdev <konnor5456@gmail.com>
2018-06-09 18:59:04 -0400

    Readded board & board tests

--- lib/paramagic_chess.rb
+++ lib/paramagic_chess.rb
@@ -1,6 +1,6 @@
 require "paramagic_chess/version"
-# require 'paramagic_chess/piece'
-# require 'paramagic_chess/tile'
+require 'paramagic_chess/piece'
+require 'paramagic_chess/tile'
 
 module ParamagicChess
   # Your code goes here...
