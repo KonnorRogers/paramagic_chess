a1f1533b997a84fc9c14a1ad695e5b69934bc6ee
paramagicdev <konnor5456@gmail.com>
2018-06-09 18:50:31 -0400

    attempting to debug tests. Deleting board & board spec

--- lib/paramagic_chess.rb
+++ lib/paramagic_chess.rb
@@ -1,7 +1,7 @@
-require "paramagic_chess/version"
-require "lib/paramagic_chess/board"
+require 'paramagic_chess/version'
 require 'paramagic_chess/piece'
 require 'paramagic_chess/tile'
+require 'paramagic_chess/board'
 
 module ParamagicChess
   # Your code goes here...
