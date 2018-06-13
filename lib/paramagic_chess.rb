9d8411342f3a959353d5fc1b02f3836f2cf932a6
paramagicdev <konnor5456@gmail.com>
2018-06-13 08:02:40 -0400

    Attempting to rework require structure.

--- lib/paramagic_chess.rb
+++ lib/paramagic_chess.rb
@@ -1,11 +1,22 @@
-require "paramagic_chess/version"
-require 'paramagic_chess/pieces/piece'
-require 'paramagic_chess/tile'
-require 'paramagic_chess/board'
+# file = "paramagic_chess"
+# pieces = file + "/pieces"
+
+# require "#{file}/version"
+# require "#{pieces}/piece"
+# require "#{file}/tile"
+# require "#{file}/board"
+
+Dir.entries('lib/paramagic_chess').each { |file| require_relative file if file =~ /\*.rb/ }
+Dir.entries('lib/paramagic_chess/pieces').each { |file| require_relative file }
+
+# Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
+
+
+# Dir["../paramagic_chess/pieces/*.rb"].each { |file| require_relative file }
 
 module ParamagicChess
   # Your code goes here...
 end
 
-game = ParamagicChess::Board.new
-game.print_board
+# game = ParamagicChess::Board.new
+# game.print_board
