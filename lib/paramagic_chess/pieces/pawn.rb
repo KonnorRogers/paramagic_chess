c5c38d49135e060b3341a71ad637c76a12fa8fee
paramagicdev <konnor5456@gmail.com>
2018-06-30 16:15:17 -0400

    Added testing for if another piece is in front of a pawn.

--- lib/paramagic_chess/pieces/pawn.rb
+++ lib/paramagic_chess/pieces/pawn.rb
@@ -12,34 +12,34 @@ module ParamagicChess
       return pawn.red if @side == :red
       'Side not set'
     end
-    
+
     def update_red_moves(board: Board.new)
       # reset possible moves
       @possible_moves = []
-      
+
       pos1 = to_pos(x: @x, y: @y - 1)
       pos2 = to_pos(x: @x, y: @y - 2)
-      
+
       # red pawns can only move -1 on the y axis
       @possible_moves << pos1 unless board.board[pos1].contains_piece?
       @possible_moves << pos2 unless moved? == true || board.board[pos1].contains_piece?
-      
+
       # checks if any enemy blue pieces at the diagonals
       possible_diagonals = blue_diagonals(board: board)
-      
+
       # #concat used to avoid array of array
       @possible_moves.concat(possible_diagonals) unless possible_diagonals.empty?
     end
-    
+
     def blue_diagonals(board: Board.new)
       x = CHAR_TO_NUM[@x]
-      
+
       possible_diagonals = []
       possible_diagonals << to_pos(x: NUM_TO_CHAR[x + 1],
-                                  y: @y - 1)
-      possible_diagonals << to_pos(x: NUM_TO_CHAR[x - 1 ],
-                                  y: @y - 1)
-      possible_diagonals.select do |pos| 
+                                   y: @y - 1)
+      possible_diagonals << to_pos(x: NUM_TO_CHAR[x - 1],
+                                   y: @y - 1)
+      possible_diagonals.select do |pos|
         # covers possible no method error case due to nil class
         next if board.board[pos].nil?
         board.board[pos].contains_blue_piece?
@@ -51,7 +51,7 @@ module ParamagicChess
       unless @possible_moves.include? pos
         return ":#{pos} is an invalid move. Try again."
       end
-      
+
       super
     end
   end
--- spec/piece_specs/pawn_spec.rb
+++ spec/piece_specs/pawn_spec.rb
@@ -28,43 +28,55 @@ module ParamagicChess
         expect(pawn.to_s).to eq 'Side not set'
       end
     end
-    
+
     context '#update_red_moves(board:)' do
       it 'provides the 2 most basic starting moves.  from a7 to a6 or a5.' do
-      
         red_pawn.update_red_moves(board: board)
-        
-        expect(red_pawn.possible_moves).to match_array [:a6, :a5]
+
+        expect(red_pawn.possible_moves).to match_array %i[a6 a5]
       end
-      
+
       it 'Will not allow for movement 2 spaces if moved = true' do
         red_pawn.move_to(pos: :a6, board: board)
         # sets @moved to true
-        
+
         red_pawn.update_red_moves(board: board)
         expect(red_pawn.possible_moves).to match_array [:a5]
       end
-      
+
       it 'allows for movement from :a7 to :b6' do
         board.board[:b6].piece = Knight.new(pos: :b6, side: :blue)
         # board.print_board
-        
+
         red_pawn.update_red_moves(board: board)
-        expect(red_pawn.possible_moves).to match_array [:a6, :a5, :b6]
+        # %i makes all symbols in the array
+        expect(red_pawn.possible_moves).to match_array %i[a6 a5 b6]
       end
-      
+
       it 'allows for movement from :b7 to :a6 & :c6' do
         board.board[:a6].piece = Knight.new(pos: :a6, side: :blue)
         board.board[:c6].piece = Pawn.new(pos: :c6, side: :blue)
-        
+
         red_pawn.update_position(pos: :b7)
         # board.print_board
-        
+
         red_pawn.update_red_moves(board: board)
-        expect(red_pawn.possible_moves).to match_array [:b6, :b5, :c6, :a6]
+        expect(red_pawn.possible_moves).to match_array %i[b6 b5 c6 a6]
+      end
+
+      it 'will return an empty array if another red piece is in front of the pawn' do
+        board.board[:a6].piece = Pawn.new(pos: :a6, side: :red)
+        red_pawn.update_red_moves(board: board)
+        expect(red_pawn.possible_moves).to be_empty
+      end
+
+      it 'will return an empty array if another blue piece is in front of the pawn' do
+        board.board[:a6].piece = Pawn.new(pos: :a6, side: :blue)
+        red_pawn.update_red_moves(board: board)
+        expect(red_pawn.possible_moves).to be_empty
       end
     end
-    
+
     context '#en_passant(pos:, board:)' do
       # TODO: later
     end
@@ -74,25 +86,25 @@ module ParamagicChess
         red_pawn.move_to(pos: :a5, board: board)
         # board.print_board
         expect(red_pawn.pos).to eq :a5
-        expect(board.board[:a5].piece).to eq red_pawn 
+        expect(board.board[:a5].piece).to eq red_pawn
       end
-      
+
       it 'removes the piece @ :b6' do
         knight_b6 = Knight.new(pos: :b6, side: :blue)
         board.board[:b6].piece = knight_b6
         red_pawn.move_to(pos: :b6, board: board)
         # board.print_board
-        
+
         expect(red_pawn.pos).to eq :b6
         expect(board.board[:b6].piece).to eq red_pawn
         expect(board.removed_blue_pieces).to match_array [knight_b6]
       end
-      
+
       it 'returns that it is an invalid move' do
         error = red_pawn.move_to(pos: :b6, board: board)
         expect(red_pawn.pos).to eq :a7
         expect(board.board[:a7].piece.type).to eq :pawn
-        expect(error).to eq ":b6 is an invalid move. Try again."
+        expect(error).to eq ':b6 is an invalid move. Try again.'
       end
     end
   end
