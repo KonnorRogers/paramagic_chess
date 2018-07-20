0b01c00246ec644817d51359488d1840d105eb36
konnor rogers <konnor7414@gmail.com>
2018-07-20 13:57:11 +0000

    added queen testing

--- spec/piece_specs/queen_spec.rb
+++ spec/piece_specs/queen_spec.rb
@@ -41,9 +41,25 @@ module ParamagicChess
         board.board[:d5].piece = queen
         queen.update_moves(board: board)
         moves = %i{d6 d4 d3 d2 c6 e6 a5 b5 c5 e5 f5 g5 h5 c4
-                    b3 a2 e4 f3 g2
-        }
+                   b3 a2 e4 f3 g2}
         expect(queen.possible_moves).to match_array moves
+      end
+    end
+    
+    context '#move_to(:board)' do
+      it 'will not move from start' do
+        queen = board.piece(pos: :e8)
+        queen.move_to(pos: :e6, board: board)
+        expect(board.piece(pos: :e8)).to eq queen
+        expect(board.piece(pos: :e6)).to eq nil
+      end
+      
+      it 'will move if the pawn in front is moved' do
+        queen = board.piece(pos: :e8)
+        board.board[:e7].piece = nil
+        queen.move_to(pos: :e2, board: board)
+        expect(board.piece(pos: :e2)).to eq queen
+        expect(board.piece(pos: :e8)).to eq nil
       end
     end
   end
