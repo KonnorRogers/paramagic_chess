fe18583bd397a68c6194c9d72bd6804ba7bf33a2
konnor rogers <konnor7414@gmail.com>
2018-07-21 01:07:25 +0000

    Players are added in game loop. Testing added.

--- lib/paramagic_chess/game.rb
+++ lib/paramagic_chess/game.rb
@@ -2,32 +2,41 @@ module ParamagicChess
   class Game
     SAFE_WORDS = [:save, :load, :castle]
     
+    attr_reader :board, :players
+    
     def initialize
       @board = Board.new
       @players = []
     end
     
+    # player1, player2, and input all added for testing purposes
     def add_players(player1: nil, player2: nil, input: nil)
-      add_player_one(player1)
-      
-      add_computer_or_player(player2: player2, input: input)
+      add_player(name: player1)
+      add_computer_or_player(player: player2, input: input)
     end
     
     def play
       greeting_message
       add_players
+      randomize_sides
+      
       # loop do
         
       # end
     end
     
-    def check
+    def randomize_sides
+      @players.shuffle!
+      @players[0].side = :red
+      @players[1].side = :blue
     end
     
     def greeting_message
       system 'clear'
       puts "Welcome to ParamagicChess. Theres nothing magical here \n
             Just Chess."
+      puts "If you would like to start from a previously
+            saved game, type 'load'"
     end
     
     private
@@ -35,22 +44,19 @@ module ParamagicChess
     def add_player(name: nil)
       puts "What is your name? \n"
       name ||= gets.chomp
-      @players << Player.new(name)
+      @players << Player.new(name: name)
     end
     
     def add_computer_or_player(player: nil, input: nil)
       loop do
+        puts "Please enter Y/N to play against a computer"
         input ||= gets.chomp.to_sym
-        if input == :y
-          # Adds a computer
-          # To be implemented later
-          break
-        elsif input == :n
-          add_player(name: player)
-          break
-        else
-          puts "Please enter Y/N to play against a computer"
-        end
+        break if input == :y
+        # Adds a computer
+        # To be implemented later
+        
+        add_player(name: player) if input == :n
+        break if input == :n
       end
     end
     
---
+++ spec/game_spec.rb
@@ -0,0 +1,34 @@
+module ParamagicChess
+  RSpec.describe Game do
+    let(:game) { Game.new }
+    
+    context '#initialize' do
+      it 'creates a board' do
+        expect(game.board).to be_an_instance_of Board
+      end
+      
+      it 'creates an empty array for players' do
+        expect(game.players).to be_empty
+      end
+      
+      it 'has an array of safe words' do
+        safe_words = %i{save load castle}
+        expect(Game::SAFE_WORDS).to match_array safe_words
+      end
+    end
+    
+    context '#add_players(player1, player2, input)' do
+      it 'will add only 1 player if playing against computer' do
+        game.add_players(player1: 'konnor', input: :y)
+        expect(game.players[0].name).to eq 'konnor'  
+        expect(game.players[1]).to eq nil
+      end
+      
+      it 'will add both players with input of :n' do
+        game.add_players(player1: 'konnor', player2: 'evelyn', input: :n)
+        expect(game.players[0].name).to eq 'konnor'
+        expect(game.players[1].name).to eq 'evelyn'
+      end
+    end
+  end
+end
